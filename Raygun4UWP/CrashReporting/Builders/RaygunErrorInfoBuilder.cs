dcb5039b9db79bb2e33cfcafd6d432c821123e9b
QuantumNightmare <quantumnightmare@hotmail.com>
2019-08-20 12:05:13 +1200

    Split native information code into distinct methods

--- Raygun4UWP/CrashReporting/Builders/RaygunErrorInfoBuilder.cs
+++ Raygun4UWP/CrashReporting/Builders/RaygunErrorInfoBuilder.cs
@@ -158,72 +158,11 @@ namespace Raygun4UWP
           
           if (images.All(i => i.BaseAddress != nativeImageBaseLong))
           {
-            // PE Format:
-            // -----------
-            // https://docs.microsoft.com/en-us/windows/win32/debug/pe-format
-            // -----------
-            // MS-DOS Stub
-            // Signature (4 bytes, offset to this can be found at 0x3c)
-            // COFF File Header (20 bytes)
-            // Optional header (variable size)
-            //   Standard fields
-            //   Windows-specific fields
-            //   Data directories (position 96/112)
-            //     ...
-            //     Debug (8 bytes, position 144/160)
-            //     ...
-
-            // TODO: use SizeOfOptionalHeader and NumberOfRvaAndSizes before tapping into data directories
-
-            // All offset values are relative to the nativeImageBase
-            int signatureOffset = CopyInt32(nativeImageBase + SIGNATURE_OFFSET_OFFSET);
-
-            int optionalHeaderOffset = signatureOffset + SIGNATURE_SIZE + COFF_FILE_HEADER_SIZE;
-
-            short magic = CopyInt16(nativeImageBase + optionalHeaderOffset);
-            
-            int debugDataDirectoryOffset = optionalHeaderOffset + (magic == (short) PEMagic.PE32 ? DEBUG_DATA_DIRECTORY_OFFSET_32 : DEBUG_DATA_DIRECTORY_OFFSET_64);
-
-            // TODO: this address can be 0 if there is no debug information:
-            int debugVirtualAddress = CopyInt32(nativeImageBase + debugDataDirectoryOffset);
-            
-            int debugSize = CopyInt32(nativeImageBase + debugDataDirectoryOffset + 4);
-
-            int debugDirectoryCount = debugSize / DEBUG_DIRECTORY_SIZE;
-
-            RaygunImageInfo image = new RaygunImageInfo
+            RaygunImageInfo imageInfo = ReadImage(nativeImageBase);
+            if (imageInfo != null)
             {
-              BaseAddress = nativeImageBaseLong
-            };
-
-            List<RaygunImageDebugInfo> debugInfo = new List<RaygunImageDebugInfo>();
-
-            for (int i = 0; i < debugDirectoryCount; i++)
-            {
-              int debugDirectoryAddress = debugVirtualAddress + (i * DEBUG_DIRECTORY_SIZE);
-              
-              int type = CopyInt32(nativeImageBase + debugDirectoryAddress + 12);
-
-              if (type == (int)DebugDirectoryEntryType.CodeView)
-              {
-                int sizeOfData = CopyInt32(nativeImageBase + debugDirectoryAddress + 16);
-
-                int addressOfRawData = CopyInt32(nativeImageBase + debugDirectoryAddress + 20);
-
-                RaygunImageDebugInfo raygunImageDebugInfo = ReadDebugInformation(nativeImageBase, addressOfRawData, sizeOfData);
-                if (raygunImageDebugInfo != null)
-                {
-                  debugInfo.Add(raygunImageDebugInfo);
-                }
-              }
+              images.Add(imageInfo);
             }
-
-            if (debugInfo.Count > 0)
-            {
-              image.DebugInfo = debugInfo.ToArray();
-            }
-            
-            images.Add(image);
           }
           
           var line = new RaygunNativeStackTraceFrame
@@ -239,16 +178,124 @@ namespace Raygun4UWP
       return lines.Count == 0 ? null : lines.ToArray();
     }
 
+    private static RaygunImageInfo ReadImage(IntPtr nativeImageBase)
+    {
+      // PE Format:
+      // -----------
+      // Reference: https://docs.microsoft.com/en-us/windows/win32/debug/pe-format
+      // -----------
+      // MS-DOS Stub
+      // Signature (4 bytes, offset to this can be found at 0x3c which is within the MS-DOS Stub)
+      // COFF File Header (20 bytes)
+      //   SizeOfOptionalHeader (2 bytes, position 16)
+      // Optional header (variable size)
+      //   Standard fields
+      //     Magic (2 bytes, position 0)
+      //   Windows-specific fields
+      //   Data directories
+      //     ...
+      //     Debug (8 bytes, position 144/160 relative to optional header)
+      //     ...
+      
+      // All offset values are relative to the nativeImageBase
+      int signatureOffset = CopyInt32(nativeImageBase + SIGNATURE_OFFSET_OFFSET);
+
+      short sizeOfOptionalHeader = CopyInt16(nativeImageBase + signatureOffset + SIGNATURE_SIZE + 16);
+
+      int optionalHeaderOffset = signatureOffset + SIGNATURE_SIZE + COFF_FILE_HEADER_SIZE;
+
+      short magic = CopyInt16(nativeImageBase + optionalHeaderOffset);
+      
+      int debugDataDirectoryOffset = optionalHeaderOffset + (magic == (short)PEMagic.PE32 ? DEBUG_DATA_DIRECTORY_OFFSET_32 : DEBUG_DATA_DIRECTORY_OFFSET_64);
+
+      if (debugDataDirectoryOffset < optionalHeaderOffset + sizeOfOptionalHeader)
+      {
+        List<RaygunImageDebugInfo> debugInfo = ReadDebugDataDirectory(nativeImageBase, debugDataDirectoryOffset);
+
+        if (debugInfo != null && debugInfo.Count > 0)
+        {
+          return new RaygunImageInfo
+          {
+            BaseAddress = nativeImageBase.ToInt64(),
+            DebugInfo = debugInfo.ToArray()
+          };
+        }
+      }
+
+      return null;
+    }
+
+    private static List<RaygunImageDebugInfo> ReadDebugDataDirectory(IntPtr nativeImageBase, int rva)
+    {
+      // Optional header data directories:
+      // ----------------------------------
+      // Reference: https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#optional-header-data-directories-image-only
+      // ----------------------------------
+      // +0    dword    the relative virtual address of the directory
+      // +4    dword    the size in bytes of the directory
+
+      int debugVirtualAddress = CopyInt32(nativeImageBase + rva);
+
+      if (debugVirtualAddress != 0)
+      {
+        int debugSize = CopyInt32(nativeImageBase + rva + 4);
+
+        int debugDirectoryCount = debugSize / DEBUG_DIRECTORY_SIZE;
+
+        return ReadDebugDirectories(nativeImageBase, debugVirtualAddress, debugDirectoryCount);
+      }
+
+      return null;
+    }
+
+    private static List<RaygunImageDebugInfo> ReadDebugDirectories(IntPtr nativeImageBase, int debugDirectoriesAddress, int debugDirectoryCount)
+    {
+      // Debug directories:
+      // -------------------
+      // Reference: https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#the-debug-section
+      // -------------------
+      // +0     dword    characteristics - reserved, must be zero
+      // +4     dword    the time and date that the debug data was created
+      // +8     word     the major version number of the debug data format
+      // +10    word     the minor version number of the debug data format
+      // +12    dword    the format of debugging information
+      // +16    dword    the size of the debug data(not including the debug directory itself)
+      // +20    dword    the address of the debug data when loaded, relative to the image base
+      // +24    dword    the file pointer to the debug data
+
+      List<RaygunImageDebugInfo> debugInfo = new List<RaygunImageDebugInfo>();
+
+      for (int i = 0; i < debugDirectoryCount; i++)
+      {
+        int debugDirectoryAddress = debugDirectoriesAddress + (i * DEBUG_DIRECTORY_SIZE);
+
+        int type = CopyInt32(nativeImageBase + debugDirectoryAddress + 12);
+
+        if (type == (int)DebugDirectoryEntryType.CodeView)
+        {
+          int sizeOfData = CopyInt32(nativeImageBase + debugDirectoryAddress + 16);
+
+          int addressOfRawData = CopyInt32(nativeImageBase + debugDirectoryAddress + 20);
+
+          RaygunImageDebugInfo raygunImageDebugInfo = ReadDebugInformation(nativeImageBase, addressOfRawData, sizeOfData);
+          if (raygunImageDebugInfo != null)
+          {
+            debugInfo.Add(raygunImageDebugInfo);
+          }
+        }
+      }
+
+      return debugInfo;
+    }
+
     private static RaygunImageDebugInfo ReadDebugInformation(IntPtr nativeImageBase, int address, int size)
     {
       // Debug information:
       // -------------------
-      // Reference: http://www.godevtool.com/Other/pdb.htm
-      // -------------------
-      // +0     dword          "RSDS" signature
-      // +4     GUID           16 - byte Globally Unique Identifier
-      // +20    dword          a value which is incremented each time the executable and its associated pdb file is remade by the linker 
-      // +24    byte string    zero terminated UTF8 path and file name
+      // +0     dword     "RSDS" signature
+      // +4     GUID      16 - byte Globally Unique Identifier
+      // +20    dword     a value which is incremented each time the executable and its associated pdb file is remade by the linker 
+      // +24    string    zero terminated UTF8 PDB path and file name
 
       int debugSignature = CopyInt32(nativeImageBase + address);
 
