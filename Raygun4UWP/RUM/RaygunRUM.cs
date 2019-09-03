de2a18d5d6d206182efb6086a6584943224a6fd0
QuantumNightmare <quantumnightmare@hotmail.com>
2019-09-03 14:04:28 +1200

    Time navigation events

--- Raygun4UWP/RUM/RaygunRUM.cs
+++ Raygun4UWP/RUM/RaygunRUM.cs
@@ -1,4 +1,4 @@
-﻿using Windows.UI.Composition;
+﻿using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -7,6 +7,8 @@ namespace Raygun4UWP
 {
   public class RaygunRUM
   {
+    private static readonly Stopwatch _stopwatch = new Stopwatch();
+
     public static readonly DependencyProperty ListenToNavigationProperty =
       DependencyProperty.RegisterAttached(
         "ListenToNavigation",
@@ -15,31 +17,46 @@ namespace Raygun4UWP
         new PropertyMetadata(false)
       );
 
-    public static void SetListenToNavigation(UIElement element, bool value)
+    /// <summary>
+    /// Causes RaygunClient.Current to listen to navigation events of the given Frame element
+    /// and send page navigation events to Raygun.
+    /// </summary>
+    /// <param name="element">A Frame element.</param>
+    /// <param name="listenToNavigation">Whether or not to listen to navigation events.</param>
+    public static void SetListenToNavigation(UIElement element, bool listenToNavigation)
     {
-      element.SetValue(ListenToNavigationProperty, value);
+      element.SetValue(ListenToNavigationProperty, listenToNavigation);
 
-      if (value)
+      if (element is Frame frame)
       {
-        if (element is Frame frame)
+        if (listenToNavigation)
         {
           frame.Navigating += Frame_OnNavigating;
           frame.Navigated += Frame_OnNavigated;
+        }
+        else
+        {
+          frame.Navigating -= Frame_OnNavigating;
+          frame.Navigated -= Frame_OnNavigated;
         }
       }
     }
 
     private static void Frame_OnNavigating(object sender, NavigatingCancelEventArgs e)
     {
-
+      if (RaygunClient.Current != null && e.SourcePageType != null)
+      {
+        _stopwatch.Restart();
+      }
     }
 
     private static void Frame_OnNavigated(object sender, NavigationEventArgs e)
     {
       if (RaygunClient.Current != null && e.SourcePageType != null)
       {
+        _stopwatch.Stop();
         string name = e.SourcePageType.Name;
-        RaygunClient.Current.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, name, 0);
+        RaygunClient.Current.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, name, _stopwatch.ElapsedMilliseconds);
       }
     }
 
