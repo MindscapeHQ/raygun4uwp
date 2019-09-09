6065a2715aed9205ae34731e22f9e35d3919a387
QuantumNightmare <quantumnightmare@hotmail.com>
2019-09-09 14:16:50 +1200

    ApplicationVersion and UserInfo tests

--- Raygun4UWP.Tests/RUM/RUMServiceTests.cs
+++ Raygun4UWP.Tests/RUM/RUMServiceTests.cs
@@ -8,12 +8,23 @@ namespace Raygun4UWP.Tests
   {
     private RUMService _rumService;
 
+    private RaygunUserInfo _customUserInfo;
+
     [TestInitialize]
     public void SetUp()
     {
       RaygunSettings settings = new RaygunSettings("");
 
       _rumService = new RUMService(settings);
+      _rumService.Enable();
+
+      _customUserInfo = new RaygunUserInfo("Ronald_Raygun");
+    }
+
+    [TestCleanup]
+    public void TearDown()
+    {
+      _rumService.Disable();
     }
 
     #region SendSessionStartEventAsync tests
@@ -57,6 +68,31 @@ namespace Raygun4UWP.Tests
       Assert.IsNull(eventInfo.Data);
     }
 
+    [TestMethod]
+    public void SendSessionStartEventAsync_CustomApplicationVersion_MessageHasSpecifiedApplicationVersion()
+    {
+      string applicationVersion = "custom-version";
+      _rumService.ApplicationVersion = applicationVersion;
+
+      RaygunRUMMessage sessionStartMessage = _rumService.SendSessionStartEventAsync().Result;
+
+      Assert.AreEqual(1, sessionStartMessage.EventData.Length);
+      RaygunRUMEventInfo eventInfo = sessionStartMessage.EventData[0];
+      Assert.AreEqual(applicationVersion, eventInfo.Version);
+    }
+
+    [TestMethod]
+    public void SendSessionStartEventAsync_CustomUserInfo_MessageHasSpecifiedUserInfo()
+    {
+      _rumService.UserInfo = _customUserInfo;
+
+      RaygunRUMMessage sessionStartMessage = _rumService.SendSessionStartEventAsync().Result;
+
+      Assert.AreEqual(1, sessionStartMessage.EventData.Length);
+      RaygunRUMEventInfo eventInfo = sessionStartMessage.EventData[0];
+      Assert.AreEqual(_customUserInfo, eventInfo.User);
+    }
+
     #endregion // SendSessionStartEventAsync tests
 
     #region SendSessionEndEventAsync tests
@@ -64,6 +100,9 @@ namespace Raygun4UWP.Tests
     [TestMethod]
     public void SendSessionEndEventAsync_NoActiveSession_ReturnsNull()
     {
+      // First, ending the session that is started via the Enable method call in the test setup
+      _rumService.SendSessionEndEventAsync().Wait();
+
       RaygunRUMMessage sessionEndMessage = _rumService.SendSessionEndEventAsync().Result;
 
       Assert.IsNull(sessionEndMessage);
@@ -97,6 +136,33 @@ namespace Raygun4UWP.Tests
       Assert.AreEqual(DefaultUserService.DefaultUser, eventInfo.User);
       Assert.AreEqual(EnvironmentService.PackageVersion, eventInfo.Version);
       Assert.IsNull(eventInfo.Data);
+    }
+
+    [TestMethod]
+    public void SendSessionEndEventAsync_CustomApplicationVersion_MessageHasSpecifiedApplicationVersion()
+    {
+      string applicationVersion = "custom-version";
+      _rumService.ApplicationVersion = applicationVersion;
+      _rumService.SendSessionStartEventAsync().Wait();
+      
+      RaygunRUMMessage sessionEndMessage = _rumService.SendSessionEndEventAsync().Result;
+
+      Assert.AreEqual(1, sessionEndMessage.EventData.Length);
+      RaygunRUMEventInfo eventInfo = sessionEndMessage.EventData[0];
+      Assert.AreEqual(applicationVersion, eventInfo.Version);
+    }
+
+    [TestMethod]
+    public void SendSessionEndEventAsync_CustomUserInfo_MessageHasSpecifiedUserInfo()
+    {
+      _rumService.UserInfo = _customUserInfo;
+      _rumService.SendSessionStartEventAsync().Wait();
+
+      RaygunRUMMessage sessionEndMessage = _rumService.SendSessionEndEventAsync().Result;
+
+      Assert.AreEqual(1, sessionEndMessage.EventData.Length);
+      RaygunRUMEventInfo eventInfo = sessionEndMessage.EventData[0];
+      Assert.AreEqual(_customUserInfo, eventInfo.User);
     }
 
     #endregion // SendSessionEndEventAsync tests
@@ -159,6 +225,86 @@ namespace Raygun4UWP.Tests
       Assert.AreEqual(type, timingData[0].Timing.Type);
       Assert.AreEqual(name, timingData[0].Name);
       Assert.AreEqual(duration, timingData[0].Timing.Duration);
+    }
+
+    [TestMethod]
+    public void SendSessionTimingEventAsync_CustomApplicationVersion_MessageHasSpecifiedApplicationVersion()
+    {
+      string applicationVersion = "custom-version";
+      _rumService.ApplicationVersion = applicationVersion;
+
+      RaygunRUMMessage sessionTimingMessage = _rumService.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, "page", 1000).Result;
+
+      Assert.AreEqual(1, sessionTimingMessage.EventData.Length);
+      RaygunRUMEventInfo eventInfo = sessionTimingMessage.EventData[0];
+      Assert.AreEqual(applicationVersion, eventInfo.Version);
+    }
+
+    [TestMethod]
+    public void SendSessionTimingEventAsync_CustomUserInfo_MessageHasSpecifiedUserInfo()
+    {
+      _rumService.UserInfo = _customUserInfo;
+
+      RaygunRUMMessage sessionTimingMessage = _rumService.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, "page", 1000).Result;
+
+      Assert.AreEqual(1, sessionTimingMessage.EventData.Length);
+      RaygunRUMEventInfo eventInfo = sessionTimingMessage.EventData[0];
+      Assert.AreEqual(_customUserInfo, eventInfo.User);
+    }
+
+    [TestMethod]
+    public void SendSessionTimingEventAsync_UserInfoChangedFromNullToCustomUserInfo_MessageHasSameSessionId()
+    {
+      RaygunRUMMessage sessionStartMessage = _rumService.SendSessionStartEventAsync().Result;
+
+      RaygunRUMEventInfo eventInfo = sessionStartMessage.EventData[0];
+      string sessionId = eventInfo.SessionId;
+
+      _rumService.UserInfo = _customUserInfo;
+
+      RaygunRUMMessage sessionTimingMessage = _rumService.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, "page", 1000).Result;
+
+      Assert.AreEqual(1, sessionTimingMessage.EventData.Length);
+      eventInfo = sessionTimingMessage.EventData[0];
+      Assert.AreEqual(sessionId, eventInfo.SessionId);
+    }
+
+    [TestMethod]
+    public void SendSessionTimingEventAsync_UserInfoChangedFromCustomUserInfoToNull_MessageHasDifferentSessionId()
+    {
+      _rumService.UserInfo = _customUserInfo;
+
+      RaygunRUMMessage sessionStartMessage = _rumService.SendSessionStartEventAsync().Result;
+
+      RaygunRUMEventInfo eventInfo = sessionStartMessage.EventData[0];
+      string sessionId = eventInfo.SessionId;
+
+      _rumService.UserInfo = null;
+
+      RaygunRUMMessage sessionTimingMessage = _rumService.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, "page", 1000).Result;
+
+      Assert.AreEqual(1, sessionTimingMessage.EventData.Length);
+      eventInfo = sessionTimingMessage.EventData[0];
+      Assert.AreNotEqual(sessionId, eventInfo.SessionId);
+    }
+
+    [TestMethod]
+    public void SendSessionTimingEventAsync_UserInfoChangedFromCustomUserInfoToDifferentCustomUserInfo_MessageHasDifferentSessionId()
+    {
+      _rumService.UserInfo = _customUserInfo;
+
+      RaygunRUMMessage sessionStartMessage = _rumService.SendSessionStartEventAsync().Result;
+
+      RaygunRUMEventInfo eventInfo = sessionStartMessage.EventData[0];
+      string sessionId = eventInfo.SessionId;
+
+      _rumService.UserInfo = new RaygunUserInfo("Different_User");
+
+      RaygunRUMMessage sessionTimingMessage = _rumService.SendSessionTimingEventAsync(RaygunRUMEventTimingType.ViewLoaded, "page", 1000).Result;
+
+      Assert.AreEqual(1, sessionTimingMessage.EventData.Length);
+      eventInfo = sessionTimingMessage.EventData[0];
+      Assert.AreNotEqual(sessionId, eventInfo.SessionId);
     }
 
     #endregion // SendSessionTimingEventAsync tests
